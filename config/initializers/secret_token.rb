072f50db1dad66b451433294b665172abc095614
Phong Nguyen <phong.nguyen@techpropulsionlabs.com>
2011-06-22 20:36:21 +0700

    New Housekeeping Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Housekeeping4::Application.config.secret_token = '8217b73bb58df8ce47454cf4537d32d44b5dee2f917ebee1adb41e62cdbbcda5778978df6028b9d5c29eefbe9d4fe905e4635e85da2ee0e031a951ab18a8eb36'
