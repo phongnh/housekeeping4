a3119b31e5d61a2b6685c6e78032f034165353b9
username <emailaddresss>
2011-07-20 14:57:12 +0700

    Add association

--- app/models/account.rb
+++ app/models/account.rb
@@ -15,4 +15,6 @@
 #
 
 class Account < ActiveRecord::Base
+  belongs_to :owner, :class_name => "User", :foreign_key => :owner_id
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :first_name, :last_name,
+                  :password, :password_confirmation, :remember_me
+
+  has_many :accounts, :foreign_key => :owner_id
 end
 
