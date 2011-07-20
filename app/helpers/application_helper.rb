4570d3fff35cdd368a5a083e00cc2024a3e0fc9c
username <emailaddresss>
2011-07-20 14:59:07 +0700

    Add human attribute name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def human_field_name(field, required=nil, klass=ActiveRecord::Base)
+    [klass.human_attribute_name(field), required].reject{|e| e.blank?}.join("")
+  end
 end
+
