5adc9d9d14566a2758c384c3c1c7f8fa7e2bf4ee
Phong Nguyen <nhphong1406@gmail.com>
2011-07-20 22:47:39 +0700

    Remove devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+#  devise :database_authenticatable, :registerable, :confirmable,
+#         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :first_name, :last_name,
