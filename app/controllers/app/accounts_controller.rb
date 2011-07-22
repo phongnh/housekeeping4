6a4433a5a2211d68e4a499e46da7ecddfbe5acf4
Phong Nguyen <nhphong1406@gmail.com>
2011-07-22 19:04:41 +0700

    View all accounts

--- app/controllers/app/accounts_controller.rb
+++ app/controllers/app/accounts_controller.rb
@@ -1,6 +1,7 @@
 class App::AccountsController < ApplicationController
   def index
-
+    @accounts = Account.includes(:owner).order([:owner_id, :name]).
+                        page(params[:page]).per(10)
   end
 end
 
