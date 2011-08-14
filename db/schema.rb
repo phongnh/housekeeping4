13e7f40407c1be7d301270d888f515e575542966
Phong Nguyen <nhphong1406@gmail.com>
2011-08-15 00:07:05 +0700

    Update database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110814152642) do
+ActiveRecord::Schema.define(:version => 20110721000000) do
 
   create_table "account_types", :force => true do |t|
     t.string "name"
